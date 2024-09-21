74e91f7ab824cc51a746ea95e6596f04a7812904
Georg Rybski <rybskigeorg@gmail.com>
2024-09-21 17:23:30 -0300

    feat: add quiz2 solution

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,29 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input.into_iter()
+            .map(|(string, command)| match command {
+                Command::Trim => string.trim().to_string(),
+                Command::Uppercase => string.to_uppercase(),
+                Command::Append(reps) => string + &"bar".repeat(reps),
+            })
+            .collect()
+
+        // input.into_iter()
+        //     .map(transform)
+        //     .collect()
+    }
+
+    // fn transform(input: (String, Command)) -> String {
+    //     let (string, command) = input;
+    //     match command {
+    //         Command::Trim => string.trim().to_string(),
+    //         Command::Append(reps) => string + &"bar".repeat(reps),
+    //         Command::Uppercase => string.to_uppercase()
+    //     }
+    // }
+
 }
 
 fn main() {
@@ -37,7 +59,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
